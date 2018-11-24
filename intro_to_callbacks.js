84b6c973035b3fa53a7245246c6ea49f466dfd1e
christine.pham <cstinepham@gmail.com>
2018-11-24 14:44:53 -0800

    Complete clock implementation

--- intro_to_callbacks.js
+++ intro_to_callbacks.js
@@ -4,17 +4,37 @@ class Clock {
     // 2. Store the hours, minutes, and seconds.
     // 3. Call printTime.
     // 4. Schedule the tick at 1 second intervals.
+    const date = new Date();
+    this.hours = date.getHours();
+    this.mins = date.getMinutes();
+    this.secs = date.getSeconds();
+
+
+    this.printTime();
+    setInterval(this._tick(), 1000)
+
   }
 
-  printTime() {
-    // Format the time in HH:MM:SS
-    // Use console.log to print it.
+   printTime() {
+    console.log(`${hours}:${mins}:${secs}`);
   }
 
-  _tick() {
-    // 1. Increment the time by one second.
-    // 2. Call printTime.
+   _tick() {
+    setInterval(printTime, 1000);
   }
+
+  incementSeconds(){
+    this.secs += 1
+  }
+
+  incrementMinutes(){
+    this.mins += 1
+  }
+
+  incrementHours(){
+    this.hours += 1
+  }
+
 }
 
 const clock = new Clock();
